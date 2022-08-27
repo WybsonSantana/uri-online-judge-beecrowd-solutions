03ec4bd4c8a2e3821bd22e106b027b3ad2f280a6
WybsonSantana <wybson@hotmail.com>
2022-08-26 23:25:04 -0300

    BEE 1007 - Diferença

---
+++ kotlin-resolutions/src/main/kotlin/bee1007/BEE1007.kt
@@ -0,0 +1,10 @@
+package bee1007
+
+fun main(args: Array<String>) {
+    val A = readLine()!!.toInt()
+    val B = readLine()!!.toInt()
+    val C = readLine()!!.toInt()
+    val D = readLine()!!.toInt()
+    val DIFERENCA =A * B - C * D
+    println("DIFERENCA = $DIFERENCA")
+}
