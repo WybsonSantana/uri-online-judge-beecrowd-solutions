6cc721fc678c45b7450a79ad5e2610675bc49188
WybsonSantana <wybson@hotmail.com>
2023-05-07 19:01:20 -0300

    BEE 2581 - I am Toorg!

---
+++ kotlin-resolutions/src/main/kotlin/bee2581/BEE2581.kt
@@ -0,0 +1,12 @@
+package bee2581
+
+import javax.swing.text.html.HTML.Attribute.N
+
+fun main(args: Array<String>) {
+    val N = readLine()!!.toInt()
+
+    repeat(N) {
+        val pergunta = readLine()!!
+        println("I am Toorg!")
+    }
+}
