685cedf2099248181d702512a561b75046687f89
WybsonSantana <wybson@hotmail.com>
2023-05-15 21:53:38 -0300

    BEE 2755 - Saída 9

---
+++ kotlin-resolutions/src/main/kotlin/bee2755/BEE2755.kt
@@ -0,0 +1,12 @@
+package bee2755
+
+fun main(args: Array<String>) {
+    println(
+        """
+            "Ro'b'er	to\/"
+            (._.) ( l: ) ( .-. ) ( :l ) (._.)
+            (^_-) (-_-) (-_^)
+            ("_") ('.')
+        """.trimIndent()
+    )
+}
