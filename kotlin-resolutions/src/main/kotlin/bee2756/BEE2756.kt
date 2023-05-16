f7d65effa34c5619d6634d8e0973f41ad946a819
WybsonSantana <wybson@hotmail.com>
2023-05-15 22:29:02 -0300

    BEE 2756 - Saída 10

---
+++ kotlin-resolutions/src/main/kotlin/bee2756/BEE2756.kt
@@ -0,0 +1,13 @@
+package bee2756
+
+fun main(args: Array<String>) {
+    println("%8s".format("A"))
+    println("%7s%2s".format("B", "B"))
+    println("%6s%4s".format("C", "C"))
+    println("%5s%6s".format("D", "D"))
+    println("%4s%8s".format("E", "E"))
+    println("%5s%6s".format("D", "D"))
+    println("%6s%4s".format("C", "C"))
+    println("%7s%2s".format("B", "B"))
+    println("%8s".format("A"))
+}
